0b9b61a0f2eed80b28f18af529a7e6744760b61d
DavidOx21 <davidv.os@hotmail.n>
2023-04-13 11:18:23 +0300

    Moving, Scaling and rotating

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./jsm/controls/OrbitControls.js";
+import { TransformControls } from "./jsm/controls/TransformControls.js";
 import { ARButton } from "./jsm/webxr/ARButton.js";
 import { GLTFLoader } from "./jsm/loaders/GLTFLoader.js";
-
-/* import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js";
-import { OrbitControls } from "./jsm/controls/OrbitControls.js";
-import { ARButton } from "./jsm/webxr/ARButton.js";
-import { GLTFLoader } from "./jsm/loaders/GLTFLoader.js";
- */
 
 let container;
 let camera, scene, renderer;
-let controller1, controller2;
-
-let raycaster;
-
-const intersected = [];
-const tempMatrix = new THREE.Matrix4();
-// Create a new GLTFLoader object
 const loader = new GLTFLoader();
-
-let group;
+let controls, transformControls;
+let model;
 
 init();
 animate();
@@ -39,9 +27,10 @@ function init() {
   );
   camera.position.set(0, 0, 3);
 
-  const controls = new OrbitControls(camera, container);
+  controls = new OrbitControls(camera, container);
   controls.minDistance = 0;
   controls.maxDistance = 8;
+  controls.enabled = true;
 
   scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
 
@@ -49,53 +38,19 @@ function init() {
   light.position.set(0, 6, 0);
   scene.add(light);
 
-  group = new THREE.Group();
-  scene.add(group);
-
-  /*   const geometries = [
-    new THREE.BoxGeometry(0.2, 0.2, 0.2),
-    new THREE.ConeGeometry(0.2, 0.2, 64),
-    new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
-    new THREE.IcosahedronGeometry(0.2, 8),
-    new THREE.TorusGeometry(0.2, 0.04, 64, 32),
-  ];
-
-  for (let i = 0; i < 50; i++) {
-    const geometry = geometries[Math.floor(Math.random() * geometries.length)];
-    const material = new THREE.MeshStandardMaterial({
-      color: Math.random() * 0xffffff,
-      roughness: 0.7,
-      metalness: 0.0,
-    });
-
-    const object = new THREE.Mesh(geometry, material);
-
-    object.position.x = Math.random() * 4 - 2;
-    object.position.y = Math.random() * 4 - 2;
-    object.position.z = Math.random() * 4 - 2;
-
-    object.rotation.x = Math.random() * 2 * Math.PI;
-    object.rotation.y = Math.random() * 2 * Math.PI;
-    object.rotation.z = Math.random() * 2 * Math.PI;
-
-    object.scale.setScalar(Math.random() + 0.5);
-
-    group.add(object);
-  } */
-
-  // Load the 3D model file
   loader.load("./models/model.gltf", function (gltf) {
-    // Get the loaded model
-    const model = gltf.scene;
-
-    // Scale down the model by 50%
+    model = gltf.scene;
     model.scale.set(0.01, 0.01, 0.01);
-
-    // Add the model to the scene
     scene.add(model);
-  });
 
-  //
+    // Initialize TransformControls after the model is loaded
+    transformControls = new TransformControls(camera, container);
+    transformControls.addEventListener("dragging-changed", function (event) {
+      controls.enabled = !event.value;
+    });
+    transformControls.attach(model);
+    scene.add(transformControls);
+  });
 
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -104,23 +59,10 @@ function init() {
   renderer.xr.enabled = true;
   container.appendChild(renderer.domElement);
 
-  document.body.appendChild(ARButton.createButton(renderer));
-
-  // controllers
-
-  controller1 = renderer.xr.getController(0);
-  controller1.addEventListener("selectstart", onSelectStart);
-  controller1.addEventListener("selectend", onSelectEnd);
-  scene.add(controller1);
-
-  controller2 = renderer.xr.getController(1);
-  controller2.addEventListener("selectstart", onSelectStart);
-  controller2.addEventListener("selectend", onSelectEnd);
-  scene.add(controller2);
-
-  raycaster = new THREE.Raycaster();
-
-  //
+  const arButton = ARButton.createButton(renderer);
+  arButton.addEventListener("sessionstart", () => (controls.enabled = false));
+  arButton.addEventListener("sessionend", () => (controls.enabled = true));
+  document.body.appendChild(arButton);
 
   window.addEventListener("resize", onWindowResize);
 }
@@ -132,76 +74,11 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function onSelectStart(event) {
-  const controller = event.target;
-
-  const intersections = getIntersections(controller);
-
-  if (intersections.length > 0) {
-    const intersection = intersections[0];
-
-    const object = intersection.object;
-    object.material.emissive.b = 1;
-    controller.attach(object);
-
-    controller.userData.selected = object;
-  }
-}
-function onSelectEnd(event) {
-  const controller = event.target;
-
-  if (controller.userData.selected !== undefined) {
-    const object = controller.userData.selected;
-    object.material.emissive.b = 0;
-    group.attach(object);
-
-    controller.userData.selected = undefined;
-  }
-}
-
-function getIntersections(controller) {
-  tempMatrix.identity().extractRotation(controller.matrixWorld);
-
-  raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
-  raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
-
-  return raycaster.intersectObjects(group.children, false);
-}
-
-function intersectObjects(controller) {
-  // Do not highlight when already selected
-
-  if (controller.userData.selected !== undefined) return;
-
-  const intersections = getIntersections(controller);
-
-  if (intersections.length > 0) {
-    const intersection = intersections[0];
-
-    const object = intersection.object;
-    object.material.emissive.r = 1;
-    intersected.push(object);
-  }
-}
-
-function cleanIntersected() {
-  while (intersected.length) {
-    const object = intersected.pop();
-    object.material.emissive.r = 0;
-  }
-}
-
-//
-
 function animate() {
-  renderer.setAnimationLoop(render);
-}
-
-function render() {
-  cleanIntersected();
-
-  intersectObjects(controller1);
-  intersectObjects(controller2);
-
-  renderer.render(scene, camera);
-}
+  renderer.setAnimationLoop(() => {
+    controls.update();
+    renderer.render(scene,
+      renderer.render(scene, camera);
+    });
+  }
+  
